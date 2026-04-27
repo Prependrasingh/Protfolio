23e34e0e49329d461cdcfcfdc82de184cb349604
prependra <prependrasingh23646knit@gmail.com>
2026-04-27 16:25:59 +0530

    commit

--- next.js
+++ next.js
@@ -4,3 +4,19 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btn = document.getElementById("theme-toggle");
+
+    btn.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+
+        if(document.body.classList.contains("dark")){
+            btn.innerText = "☀️";
+        } else {
+            btn.innerText = "🌙";
+        }
+    });
+
+});
